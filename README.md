12d8af42cb87403c8ca6677ae6705a80d7f4a443
Gary Montgomery <54603459+garysm@users.noreply.github.com>
2022-02-25 15:43:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# riverpod_template
+
+This Flutter template contains most of my common dependencies as well as my go-to state management solution, Riverpod.
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+    );
+  }
+}
