8a5404c58fe3397791b865f98837bb3c8e3463fa
garysm <gary@sabiasoftware.com>
2022-03-10 15:13:02 -0500

    set orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_template/src/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(
     const ProviderScope(child: MyApp()),
   );
